97282d25f8544f90c5ceec09bf4ce3381a3c06dd
KyleYueye <stillyueye@qq.com>
2020-06-25 21:17:08 +0800

    add spectual_cluster to fastspfa

--- src/mgm_floyd.py
+++ src/mgm_floyd.py
@@ -15,32 +15,6 @@ def afnty_scr(X, K, max_afnty):
     return affinity_score[0][0] / max_afnty
 
 
-def old_consistency_pair(X):
-    """
-    old version of computing consistency pair
-    :param X: matching result permutation matrix (m, m, n, n)
-    :return: consistency pair (m, m)
-    """
-    m, _, n, _ = X.shape
-    store_x = [i[0] for i in store]
-    store_y = [i[1] for i in store]
-    for i in range(m):
-        for j in range(m):
-            cnt = 0.0
-            X_ij = X[i, j]
-            if i in store_x or j in store_y:
-                pass
-            else:
-                continue
-            for k in range(m):
-                # X_ikj = X[i, k] * X[k, j]
-                if ((i, k) not in store) and ((k, j) not in store):
-                    continue
-                cnt += np.sum(np.abs(X_ij - X[i, k] * X[k, j]))
-            con_pair[i, j] = 1 - cnt / (2 * m * n)
-    return con_pair
-
-
 def consistency_pair(X):
     """
     new version of computing consistency pair by matrix operation
@@ -71,14 +45,14 @@ def mgm_floyd(X, K, num_graph, num_node):
     # for i in range(num_graph):
     #     for j in range(num_graph):
     #         afnty[i, j] = afnty_scr(X[i, j], K[i, j], 1.0)
-    
+
     X = X.transpose(0, 1, 3, 2)
     pro_X = X.reshape(num_graph, num_graph, -1, 1)
     pro_X_t = X.reshape(num_graph, num_graph, 1, -1)
     afnty = np.matmul(np.matmul(pro_X_t, K), pro_X).reshape(num_graph, num_graph)
     max_afnty = np.max(afnty)
 
-    c = 0.8
+    c = 0.4
     for k in range(num_graph):
         consistency = consistency_pair(X)
         for i in range(num_graph):
--- utils/evaluation.py
+++ utils/evaluation.py
@@ -18,3 +18,72 @@ def evaluate_matching_accuracy(mat, mat_gt, num_graph, num_inlier):
             acc = np.sum(np.sum(np.abs(x - x_gt), 1) == 0) / num_inlier
             acc_list.append(acc)
     return mean(acc_list)
+
+
+def evaluate_cluster_accuracy(cluster, cluster_gt, num_clusters, num_graphs_cluster):
+    """
+    :param cluster: an numpy array where cluster[i] tells the cluster g_i belong to, (num_graph, 1)
+    :param cluster_gt: ground truth an numpy array where cluster_gt[i] tells the cluster g_i belong to, (num_graph, 1)
+    :param num_clusters: number of clusters, int
+    :param num_graphs_cluster: number of graphs in each clusters, int
+    :return: Clustering Purity, Rand Index, Clustering Accuracy
+    """
+    assert cluster.shape == cluster_gt.shape, "cluster shape is supposed to be {}, but got {}" \
+        .format(cluster_gt.shape, cluster.shape)
+    cluster = cluster.astype(int)
+    cluster_gt = cluster_gt.astype(int)
+    cp = get_cp(cluster, cluster_gt, num_clusters, num_graphs_cluster)
+    ri = get_ri(cluster, cluster_gt, num_clusters, num_graphs_cluster)
+    ca = get_ca(cluster, cluster_gt, num_clusters, num_graphs_cluster)
+    return cp, ri, ca
+
+
+def get_cp(cluster, cluster_gt, num_clusters, num_graphs):
+    tot = 0
+    for i in range(num_clusters):
+        cnt = np.zeros((num_clusters, 1))
+        for j in range(num_graphs):
+            if cluster[j] == i:
+                cnt[cluster_gt[j]] += 1
+        mx = np.max(cnt)
+        tot = tot + mx
+    cp = tot / num_graphs
+    return cp
+
+
+def get_ri(cluster, cluster_gt, num_clusters, num_graphs):
+    tot = 0
+    tp = 0
+    for i in range(num_graphs):
+        for j in range(i + 1, num_graphs):
+            tot = tot + 1
+            if (cluster[i] == cluster[j] and cluster_gt[i] == cluster_gt[j]) or \
+                    (cluster[i] != cluster[j] and cluster_gt[i] != cluster_gt[j]):
+                tp = tp + 1
+    ri = tp / tot
+    return ri
+
+
+def get_ca(cluster, cluster_gt, num_clusters, num_graphs):
+    ca = 0
+    tot = np.zeros((num_clusters))
+    for i in range(num_graphs):
+        tot[cluster_gt[i]] += 1
+
+    for i_cluster in range(num_clusters):
+        cnt1 = np.zeros((num_clusters))
+        cnt2 = np.zeros((num_clusters))
+        for i in range(num_graphs):
+            if cluster_gt[i] == i_cluster:
+                cnt1[cluster[i]] += 1
+            if cluster[i] == i_cluster:
+                cnt2[cluster_gt[i]] += 1
+        for i in range(num_clusters):
+            for j in range(i + 1, num_clusters):
+                if tot[i_cluster] > 0:
+                    ca += (cnt1[i] * cnt1[j]) / tot[i_cluster] ** 2
+                if tot[i] > 0 and tot[j] > 0:
+                    ca += (cnt2[i] * cnt2[j]) / (tot[i] * tot[j])
+
+    ca = 1 - ca / num_clusters
+    return ca
